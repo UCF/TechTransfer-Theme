907927d0b286fb0729483b6aa7cd5e32cbd0a0dd
Brandon Groves <brandon.groves@gmail.com>
2013-10-14 14:16:48 -0400

    update university header

--- functions.php
+++ functions.php
@@ -10,4 +10,16 @@ require_once('third-party/enable-media-replace/enable-media-replace.php');  # In
 
 //Add theme-specific functions here.
 
+/**
+ * Add ID attribute to registered University Header script.
+ **/
+function add_id_to_ucfhb($url) {
+    if ( (false !== strpos($url, 'bar/js/university-header.js')) || (false !== strpos($url, 'bar/js/university-header-full.js')) ) {
+      remove_filter('clean_url', 'add_id_to_ucfhb', 10, 3);
+      return "$url' id='ucfhb-script";
+    }
+    return $url;
+}
+add_filter('clean_url', 'add_id_to_ucfhb', 10, 3);
+
 ?>
