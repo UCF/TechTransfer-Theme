0e7c5ab5d32d83222830709414b9e4d5b4b6916d
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2014-04-04 15:44:14 -0400

    TechLocator: add scroll to top

--- shortcodes.php
+++ shortcodes.php
@@ -608,7 +608,27 @@ function sc_license_post_type_search($params=array(), $content='') {
                     <? } ?>
             <?
         }
-        ?> </div> <?
+        ?> </div>
+        <script>
+			jQuery(document).ready(function() {
+				var offset = 220;
+				var duration = 500;
+				jQuery(window).scroll(function() {
+					if (jQuery(this).scrollTop() > offset) {
+						jQuery('.back-to-top').fadeIn(duration);
+					} else {
+						jQuery('.back-to-top').fadeOut(duration);
+					}
+				});
+				jQuery('.back-to-top').click(function(event) {
+					event.preventDefault();
+					jQuery('html, body').animate({scrollTop: 0}, duration);
+					return false;
+				});
+		});
+		</script>
+        <a href="#" class="back-to-top">Back to Top</a>
+    <?php
 	return ob_get_clean();
 }
 add_shortcode('license-post-type-search', 'sc_license_post_type_search');
