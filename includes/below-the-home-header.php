4c84cc15f13e5fc979f504abd9d118546c90555b
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2014-04-16 14:03:46 -0400

    BelowHomeHeader: Add blog and success stories
    Remove inventor and technology posts

--- includes/below-the-home-header.php
+++ includes/below-the-home-header.php
@@ -1,26 +1,27 @@
 <div class="row">
 	<div id="below-the-home-header" class="row-border-bottom-top">
 		<div class="span4">
-			<?php $inventorPosts = new WP_Query(array( 'post_type' => 'inventor' , 'posts_per_page' => 1 )); ?>
-			<?php while($inventorPosts->have_posts()) : $inventorPosts->the_post(); ?>
-				<a href="<?=get_permalink( $post->ID ); ?>">
-				<?=the_post_thumbnail( array( 300, 300 )); ?>
-				<h2>Inventor: <b><?=the_title(); ?></b></h2>
-				<?php if($shortDescription = get_post_meta($post->ID, 'inventor_short_description', true)) : ?>
-				<p><?=$shortDescription; ?></p>
+			<?php $blog = get_page_by_path('blog'); ?>
+			<a href="<?=get_page_link($blog->ID); ?>">
+				<?php $aboutPage = new WP_Query(array( 'post_type' => 'post', 'posts_per_page' => 1 )); ?>
+				<?php while($aboutPage->have_posts()) : $aboutPage->the_post(); ?>
+					<?=the_post_thumbnail( array( 300, 300 ) ); ?>
+					<h2>Blog: <b><?=the_title(); ?></b></h2>
+				<?php if($shortDescription = get_post_meta($post->ID, 'post_short_description', true)) : ?>
+						<p><?=$shortDescription; ?></p>
 				<?php else : ?>
-				<p><?=get_the_content(); ?></p>
-				<?php endif; ?>
-				</a>
-			<?php endwhile; ?>
+						<p><?=get_the_content(); ?></p>
+				<?php endif;
+				endwhile; ?>
+			</a>
 		</div>
 		<div class="span4">
-			<?php $technologyPosts = new WP_Query(array( 'post_type' => 'technology' , 'posts_per_page' => 1 )); ?>
-			<?php while($technologyPosts->have_posts()) : $technologyPosts->the_post(); ?>
+			<?php $sucessStoryPosts = new WP_Query(array( 'post_type' => 'success_story' , 'posts_per_page' => 1 )); ?>
+			<?php while($sucessStoryPosts->have_posts()) : $sucessStoryPosts->the_post(); ?>
 				<a href="<?=get_permalink( $post->ID ); ?>">
-				<?=the_post_thumbnail( array( 300, 300 ) ); ?>
-				<h2>Technology: <b><?=the_title(); ?></b></h2>
-				<?php if($shortDescription = get_post_meta($post->ID, 'technology_short_description', true)) : ?>
+				<?=the_post_thumbnail( array( 300, 300 )); ?>
+				<h2>Success Story: <b><?=the_title(); ?></b></h2>
+				<?php if($shortDescription = get_post_meta($post->ID, 'success_story_short_description', true)) : ?>
 				<p><?=$shortDescription; ?></p>
 				<?php else : ?>
 				<p><?=get_the_content(); ?></p>
