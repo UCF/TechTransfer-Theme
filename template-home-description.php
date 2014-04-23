9ab972538ab99c65cee87b9d9b2e49eac001dd85
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2014-04-23 15:22:50 -0400

    HomeHeader: remove buttons

--- template-home-description.php
+++ template-home-description.php
@@ -5,23 +5,13 @@
 	<?php $pageID  = $page->ID; ?>
 	<div class="page-content" id="home" data-template="home-description">
 		<div class="row">
-			<div class="span8">
+			<div class="span12">
 				<?php $description = $options['site_description']; ?>
 				<?php if ($description): ?>
 					<div id="tt-header-description">
 						<p><?=$description?></p>
 					</div>
 				<?php endif; ?>
-			</div>
-			<div class="span4">
-				<div id="for-researchers-button">
-					<?php $forResearchers = get_page_by_path('for-researchers'); ?>
-					<a href="<?=get_page_link($forResearchers->ID); ?>" class="btn btn-default btn-home">Researchers</a>
-				</div>
-				<div id="for-industry-button">
-					<?php $forIndustry = get_page_by_path('for-industry'); ?>
-					<a href="<?=get_page_link($forIndustry->ID); ?>" class="btn btn-default btn-home">Industry</a>
-				</div>
 			</div>
 		</div>
 		<?php get_template_part('includes/below-the-home-header'); ?>
