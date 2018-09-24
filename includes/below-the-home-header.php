9f76da0e0d76d0ee63d811939fddeb638ac41d66
Cadie Brown <cadie.brown@gmail.com>
2018-09-24 09:07:29 -0400

    Update col heading name and rearrange columns

--- includes/below-the-home-header.php
+++ includes/below-the-home-header.php
@@ -5,10 +5,27 @@
 <div class="row">
 	<div id="below-the-home-header" class="row-border-bottom-top">
 		<div class="span4">
+		<?php $newsPage = new WP_Query(array( 'post_type' => 'news', 'posts_per_page' => 1 )); ?>
+		<?php while($newsPage->have_posts()) : $newsPage->the_post(); ?>
+				<a href="<?=get_permalink( $post->ID ); ?>">
+				<h2>Featured Researcher</h2>
+				<?=the_post_thumbnail(array( 300, 300 )); ?>
+				<b><?=the_title(); ?></b>
+				<?php if($shortDescription = get_post_meta($post->ID, 'news_short_description', true)) : ?>
+				<p><?=$shortDescription; ?></p>
+				<?php else : ?>
+				<p><?=get_the_content(); ?></p>
+				<?php endif; ?>
+				</a>
+				<a href="<?=get_page_link($archives->ID); ?>">&raquo; Researcher Archives</a>
+		<?php endwhile; ?>
+		</div>
+
+		<div class="span4">
 		<?php $blogPage = new WP_Query(array( 'post_type' => 'post', 'posts_per_page' => 1 )); ?>
 		<?php while($blogPage->have_posts()) : $blogPage->the_post(); ?>
 			<a href="<?=get_page_link($blog->ID); ?>">
-				<h2>Patent Trending Blog</h2>
+				<h2>Featured Technology</h2>
 					<?=the_post_thumbnail( array( 300, 300 ) ); ?>
 					<b><?=the_title(); ?></b>
 				<?php if($shortDescription = get_post_meta($post->ID, 'post_short_description', true)) : ?>
@@ -35,23 +52,6 @@
 				<?php endif; ?>
 				</a>
 				<a href="<?=get_page_link($archives->ID); ?>">&raquo; Tech News Archives</a>
-		<?php endwhile; ?>
-		</div>
-
-		<div class="span4">
-		<?php $newsPage = new WP_Query(array( 'post_type' => 'news', 'posts_per_page' => 1 )); ?>
-		<?php while($newsPage->have_posts()) : $newsPage->the_post(); ?>
-				<a href="<?=get_permalink( $post->ID ); ?>">
-				<h2>Featured Researcher</h2>
-				<?=the_post_thumbnail(array( 300, 300 )); ?>
-				<b><?=the_title(); ?></b>
-				<?php if($shortDescription = get_post_meta($post->ID, 'news_short_description', true)) : ?>
-				<p><?=$shortDescription; ?></p>
-				<?php else : ?>
-				<p><?=get_the_content(); ?></p>
-				<?php endif; ?>
-				</a>
-				<a href="<?=get_page_link($archives->ID); ?>">&raquo; Researcher Archives</a>
 		<?php endwhile; ?>
 		</div>
 	</div>
